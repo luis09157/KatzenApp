bf9a0f4f4421c254a1c61204b6c926226640dabe
Luis Alfonso Niño Martínez <luisk2ify@gmail.com>
2023-08-02 17:57:32 -0600

    gasolina y tarjeta

--- app/src/main/java/com/example/katzen/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/katzen/ui/home/HomeFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.katzen.R
 import com.example.katzen.databinding.FragmentHomeBinding
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.cos
 
 class HomeFragment : Fragment() {
 
@@ -25,11 +30,41 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.btnCalcula.setOnClickListener {
+            if(binding.etCosto.text.toString().trim().isEmpty()){
+                Toast.makeText(context,"Agrega la cantidad a calcular.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            calcular(binding.etCosto.text.toString().toDouble())
+        }
+
+        binding.btnClean.setOnClickListener {
+            clean()
+        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun calcular(costo : Double){
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+
+        val porcentaje =  0.045 * costo
+
+        binding.txtCosto.text = "$ " + df.format(costo)
+        binding.txtGanancia.text = "$ " + df.format(porcentaje)
+        binding.txtVenta.text = "$ " + df.format(costo + porcentaje)
+    }
+
+    fun clean(){
+        binding.txtCosto.text = getString(R.string.title_dinero)
+        binding.txtGanancia.text = getString(R.string.title_dinero)
+        binding.txtVenta.text = getString(R.string.title_dinero)
+        binding.etCosto.text.clear()
     }
 }
